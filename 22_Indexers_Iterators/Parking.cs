0f26d417e51869e756a2f3c8d262fa605a4bd823
Andrey Pryahin <apryahin@gmail.com>
2020-04-28 07:21:43 +0300

    #22 End

--- 22_Indexers_Iterators/Parking.cs
+++ 22_Indexers_Iterators/Parking.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
@@ -8,7 +9,7 @@ using System.Xml.Schema;
 
 namespace _22_Indexers_Iterators
 {
-    class Parking
+    class Parking : IEnumerable
     {
 
         private List<Car> _cars = new List<Car>();
@@ -22,6 +23,27 @@ namespace _22_Indexers_Iterators
                 return car;
             }
 
+        }
+
+        public Car this[int position]
+        {
+
+            get
+            {
+                if (position < _cars.Count)
+                { 
+                    return _cars[position];
+                }
+                return null;
+            }
+
+            set
+            {
+                if (position < _cars.Count)
+                {
+                    _cars[position] = value;
+                }
+            }
         }
 
         public int Count => _cars.Count; //быстрое св-во (только для чтения). МОжно объявить доступ к закрытому полю класса только для чтения.
@@ -59,6 +81,50 @@ namespace _22_Indexers_Iterators
                 _cars.Remove(car);
             }    
         }
-        
+
+
+        public IEnumerable GetNumbers(int max)
+        {
+            var current = 0;
+            for (int i = 0; i < max; i++)
+            {
+                current += i;
+                yield return current;
+            }
+        }
+
+        public IEnumerable GetNames()
+        {
+            foreach (var car in _cars)
+            {
+                yield return car.Name;
+            }
+        }
+
+
+        public IEnumerator<Car> GetEnumerator() //просто IEnumerator Будет возвращать Object - не будут доступны св-ва объекта Car
+        {
+            return _cars.GetEnumerator();
+        }
+
+        //ienumerator ienumerable.getenumerator()
+        //{
+        //    return _cars.getenumerator();
+        //}
     }
+
+//    public class ParkingEnumerator : IEnumerator
+//    {
+//        public object Current => throw new NotImplementedException();
+
+//        public bool MoveNext()
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public void Reset()
+//        {
+//            throw new NotImplementedException();
+//        }
+//    }
 }
--- 22_Indexers_Iterators/Program.cs
+++ 22_Indexers_Iterators/Program.cs
@@ -17,10 +17,26 @@ namespace _22_Indexers_Iterators
             };
 
             var parking = new Parking();
-
+            
             foreach (var car in cars)
             {
                 parking.Add(car);
+            }
+
+            foreach (var car in parking)
+            {
+                Console.WriteLine(car);
+            }
+
+            foreach (var num in parking.GetNumbers(10))
+            {
+                Console.WriteLine(num);
+            }
+
+
+            foreach (var name in parking.GetNames())
+            {
+                Console.WriteLine("Name:" + name);
             }
 
             Car SearchedCar = parking["A001AA01"];
@@ -28,6 +44,11 @@ namespace _22_Indexers_Iterators
             SearchedCar = parking["A001AA02"];
             Console.WriteLine(SearchedCar?.Name);
 
+            Console.WriteLine("Введите номер нового автомобиля");
+            var a = Console.ReadLine();
+
+            parking[1] = new Car() { Name = "Audi", Number = a };
+            Console.WriteLine(parking[1]);
 
             //Console.WriteLine(parking._cars[0]);
             Console.Read();
