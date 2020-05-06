f67017722fe230866d2fa8caf24fa319b1da1fcf
Andrey Pryahin <apryahin@gmail.com>
2020-05-06 04:14:25 +0300

    29_Object

---
+++ 29_object/Point.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _29_object
+{
+    class Point
+    {
+        public int X { get; set; }
+
+        public override bool Equals(object obj) //внимание - принмает любой тип ,а не только Point
+                                                //правила переопределения Equals 
+                                                //1. методе не должен выкидывать Exceptions (Null)
+                                                //2. a=a
+                                                //3. (a=b, b=c) => a=c
+                                                //4. переопределяя equals -> переопрелели и оператор "="
+        {
+            return base.Equals(obj);
+            {
+                // if (obj == null) return false; - так же не нужно, тк if покрывает проверку на null
+
+                //приводим к исходному типу Poing, чтобы не сравнивать "сладкое с зеленым".
+
+                // as делает попытку преобразования к типу ,если не удается - то null.
+                if (obj is Point point)
+                {
+                    return this.X == point.X; // сравниваем значения полей для объектов, а не адреса ссылок в памяти.
+                }
+                else
+                {
+                    return false;
+                }
+
+
+
+            }
+
+        }
+        //GetHashCode() //должна быть быстрой. если Хэшкоды разные - то это точно разные обхекты. Но если одинаковые, то не факт, что один объект
+        public override int GetHashCode()
+        {
+            return X;
+        }
+
+    }
+}
---
+++ 29_object/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace _29_object
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object obj = new object();
+
+            //Equals
+
+            int i = 5;
+            int j = 5;
+
+            i.Equals(j);
+
+            //boxing- unboxing. boxing - поместить значимый объект в кучу. мы хотим работать с значимым объектом как с сылочным. например чтобы не указывать ref при 
+            // передаче в метод. (не будет происходить клонирование объекта).
+
+            Console.WriteLine(i.Equals(j));
+            object oi = (object)i; //   
+            var oj = (object)j;
+
+            var p1 = new Point() { X = 5 };
+            var p2 = new Point() { X = 5 };
+            
+            Console.WriteLine(oi.Equals(oj)); //.NET уменее чем кажется - оказалось True
+            Console.WriteLine(p1.Equals(p2)); // вот тут всё ОК - false, хотя внутренности объета идентичны
+
+            Console.WriteLine(new Class1().GetHashCode());
+            Console.WriteLine(oj.GetHashode()); //видит, что это int, запакованный в object. поэтому выдает значение 5.
+            Console.WriteLine(p1.GetHashCode());
+
+            Console.ReadLine();
+
+            
+
+
+        }
+
+    }
+}
