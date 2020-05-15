0bd347e772f840094948796bab451dfb8e407c2d
Andrey Pryahin <apryahin@gmail.com>
2020-05-15 06:14:18 +0300

    LINQ

---
+++ 20_LINQ/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20_LINQ
+{
+    class Product
+    {
+        public string Name { get; set; }
+
+        public int Energy { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} ({Energy})";
+        }
+    }
+}
---
+++ 20_LINQ/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _20_LINQ
+{
+    class Program
+    {
+        static Random rnd = new Random();
+        static void Main(string[] args)
+        {
+            var collection = new List<int>();
+
+            for (var i = 0; i < 10; i++)
+            {
+                collection.Add(i);
+            }
+
+            //для того чтобы собственный класс мог работать с LINQ необходимо чтобы он реализовал метод IEnumerable()
+            var result = from item in collection
+                         where item < 5
+                         select item;
+
+            var result2 = collection.Where(item => item < 5) //с помощью методов расширения
+                .Where(item => item % 2 == 0)
+                .OrderByDescending(item => item);
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine();
+
+            foreach (var item in result2)
+            {
+                Console.WriteLine(item);
+            }
+                       
+            var collection2 = new List<Product>();
+            for (var i = 0; i < 10; i++)
+            {
+                var product = new Product()
+                {
+                    Name = "Продукт" + i,
+                    Energy = rnd.Next(10, 500)
+                };
+                collection2.Add(product);
+            }
+
+            var result3 = from item in collection2
+                          where item.Energy > 100 && item.Energy <200
+                          select item;
+
+            foreach (var item in result3)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
